d2ac3991b80545628d94cd7f933e7ffad8a9983f
karthikeyankkn <connecttokkn@hotmail.com>
2017-12-12 01:10:11 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	// variable declarations
+	// preload funcitons
+	windowHight();
+
+	$( window ).resize(function() {
+		windowHight();
+	});
+	// window height
+	function windowHight(){
+		var window_height = $(window).height();
+		$(".banner-container").css("height", window_height);
+	}
+
+	// slider1 code
+	  $("#left_go").click(function(){
+	   // var pos = $('.slider-parent').scrollRight() + 260;
+	   //  $('.slider-parent').scrollRight(pos);
+	    var pos = $('#slider1').scrollLeft() - 284;
+	    $('#slider1').animate({scrollLeft: pos}, 300);
+	  });
+	  $("#right_go").click(function(){
+	    // debugger
+	    var pos = $('#slider1').scrollLeft() + 284;
+	    $('#slider1').animate({scrollLeft: pos}, 300);
+	    
+	  });
+
+	  // hamburger menu code
+	  $('#hamburger, .header-menu ul li a').click(function(){
+	    toggleMenu();
+	  });
+
+	  function toggleMenu(){
+	  	$('#hamburger').toggleClass('open');
+	    $("header").toggleClass("open-menu");
+	    $("body").toggleClass("stop-scrolling");
+	  }
+	 
+});
