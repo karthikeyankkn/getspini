ce99a86927c9ea61e43b3cbf5af705b759698bc0
karthikeyankkn <connecttokkn@hotmail.com>
2017-12-28 18:35:55 +0530

    added popup for spini page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 	// variable declarations
-	// preload funcitons
-	// windowHight();
 
-	// $( window ).resize(function() {
-	// 	windowHight();
-	// });
+	// preload funcitons
+	windowHight();
+
+	$( window ).resize(function() {
+		windowHight();
+	});
 	// window height
-	// function windowHight(){
-	// 	var window_height = $(window).height();
-	// 	$(".banner-container").css("height", window_height;
-	// }
+	function windowHight(){
+		var window_height = $(window).height();
+		// $(".banner-container").css("height", window_height);
+		$(".spini-popup-container").css("height", window_height);
+	}
 
 	// load header and footer
 	if (index_page == true) {
@@ -21,33 +23,34 @@ $(document).ready(function(){
   		$("footer").load("../footer.html");
 	}
 
+	// spini-popup-scripts
+	$(".spini-popup-ans span").click(function(){
+		$(this).toggleClass("active-answer").siblings().removeClass("active-answer");
+		$(this).children("input[type='radio']").prop('checked',true);
+	});
 
-	// slider1 code
-	  $("#left_go").click(function(){
-	   // var pos = $('.slider-parent').scrollRight() + 260;
-	   //  $('.slider-parent').scrollRight(pos);
-	    var pos = $('#slider1').scrollLeft() - 284;
-	    $('#slider1').animate({scrollLeft: pos}, 300);
-	  });
-	  $("#right_go").click(function(){
-	    // debugger
-	    var pos = $('#slider1').scrollLeft() + 284;
-	    $('#slider1').animate({scrollLeft: pos}, 300);
-	    
-	  });
-
-	  // slider2 code
-	  $("#left_go2").click(function(){
-	   // var pos = $('.slider-parent').scrollRight() + 260;
-	   //  $('.slider-parent').scrollRight(pos);
-	    var pos = $('#slider2').scrollLeft() - 284;
-	    $('#slider2').animate({scrollLeft: pos}, 300);
-	  });
-	  $("#right_go2").click(function(){
-	    // debugger
-	    var pos = $('#slider2').scrollLeft() + 284;
-	    $('#slider2').animate({scrollLeft: pos}, 300);
-	    
-	  });
+	// navigation script
+	$('.next').click(function(){
+      $(this).parents("li").hide();
+      $(this).parents("li").next().fadeIn();
+      var lastindex = $(this).parent().siblings();
+      var index = $(this).parents("li").index();
+      if(index == lastindex.length){  
+        $(this).prop({disabled: true});
+        $(this).parents("li").show();
+       }
+    });
+    
+    $('.back').click(function(){
+    	debugger
+      $(this).parents("li").hide();
+      $(this).parents("li").prev().fadeIn();
+      var index = $(this).parents("li").index();
+      if(index == 0){  
+        $(this).prop({disabled: true});
+        $(this).parents("li").show();
+       }
+    });
+	
 	 
 });
