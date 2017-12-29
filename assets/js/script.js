bbd708f359efff4b80f17a39b6e45b1b9f05771f
karthikeyankkn <connecttokkn@hotmail.com>
2017-12-29 23:36:27 +0530

    added reusable scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	// variable declarations
-
+  var name, ph_number, email;
+  var qa1, qa2, qa3, qa4, qa5, qa6;
+  var short_traverse;
 	// preload funcitons
 	windowHight();
 
@@ -29,7 +31,8 @@ $(document).ready(function(){
   		$("footer").load("footer.html");
 	}else{
 		$("header").load("../header.html"); 
-  		$("footer").load("../footer.html");
+  	$("footer").load("../footer.html");
+    $(".alerts_page").load("../alerts.html");
 	}
 
 	// spini-popup-scripts
@@ -62,47 +65,60 @@ $(document).ready(function(){
        }
     });
 
-    // interior popup
-    $("#interior_menu").click(function(){
-    	$(this).siblings(".toggle-page-container").fadeToggle();
+    // open popup
+    $(".spini-sep-services ul li a").click(function(){
+    	$(this).siblings(".toggle-page-container").fadeIn();
     });
 
     $(".submit").click(function(){
-    	debugger
-    	var ans1 = $("input[name='what_services']:checked").val();
-    	var ans2 = $("input[name='what_kind']:checked").val();
-    	var ans3 = $("input[name='how_old']:checked").val();
-    	var ans4 = $("input[name='what_carpet']:checked").val();
-    	
-    	// contact form script
-    	var name = $("input[name='name']").val();
-    	var ph_number = $("input[name='ph_number']").val();
-    	var email = $("input[name='email']").val();
-    	if(name == "" && ph_number == "" && email == "") {
-    		$(".error-mandatory").fadeIn();
-    	}else if(ph_number.length != 10){
-    		$(".error-contact-ph_number").fadeIn()
-    	}else if (email != "" && email != undefined) {
-    		if (validateEmail(email)) {
-				success_contact_form();
-			}else{
-				$(".alert-danger").hide();
-				$(".error-contact-email").fadeIn();
-			}
-    	}else{
-    		success_contact_form();
-    	}
-
-    	function success_contact_form(){
-    		$(".alert-danger").hide();
-    		$(".success-contact, .loading_image").fadeIn();
-    		setTimeout(function(){ 
-    			location.reload();
-    		}, 3000);
-    		$(".submit").hide();
-    	}
-    	alert(ans1+""+ans2+""+ans3+""+ans4+""+name+""+ph_number+""+email);
+    	interior_qa();
+      short_traverse = $(this).parents(".toggle-page-container ul li");
+      contact_form();
+    	alert(qa1+""+qa2+""+qa3+""+qa4+""+name+""+ph_number+""+email);
     });
+
+    // contact form script starts
+    function contact_form(){
+      debugger
+      name = short_traverse.find("input[type='text']").val();
+      ph_number = short_traverse.find("input[type='number']").val();
+      email = short_traverse.find("input[type='email']").val();
+
+      // validation script
+      if(name == "" && ph_number == "" && email == "") {
+        $(".error-mandatory").fadeIn();
+      }else if(ph_number.length != 10){
+        $(".error-contact-ph_number").fadeIn()
+      }else if (email != "" && email != undefined) {
+        if (validateEmail(email)) {
+        success_contact_form();
+      }else{
+        $(".alert-danger").hide();
+        $(".error-contact-email").fadeIn();
+      }
+      }else{
+        success_contact_form();
+      }
+    }
+    // success contact form script
+    function success_contact_form(){
+      $(".alert-danger").hide();
+      $(".success-contact, .loading_image").fadeIn();
+      setTimeout(function(){ 
+        location.reload();
+      }, 3000);
+      $(".submit").hide();
+      }
+    // contact form script ends
+
+    // questions for interior starts
+    function interior_qa(){
+      qa1 = $("input[name='what_services']:checked").val();
+      qa2 = $("input[name='what_kind']:checked").val();
+      qa3 = $("input[name='how_old']:checked").val();
+      qa4 = $("input[name='what_carpet']:checked").val();
+    }
+    // questions for interior ends
 	
 	 
 });
