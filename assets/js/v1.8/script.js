409b58d2821a82427076a181319abcf860713276
karthikeyankkn <connecttokkn@hotmail.com>
2018-02-06 13:58:53 +0530

    footer renamed

--- assets/js/v1.8/script.js
+++ assets/js/v1.8/script.js
@@ -45,10 +45,10 @@ $(document).ready(function(){
 	// load header and footer
 	if (index_page == true) {
 		$("header").load("header.html"); 
-  		$("footer").load("footer.html");
+  		$("footer").load("footer1.html");
 	}else{
 		$("header").load("../header.html"); 
-  	$("footer").load("../footer.html");
+  	$("footer").load("../footer1.html");
     $(".alerts_page").load("../alerts.html");
     // $(".contact-us").load("../contactus.html");
 	}
@@ -446,7 +446,7 @@ $(document).ready(function(){
       get_json();
       // debugger
       function get_json(index){
-        $.get("../assets/js/v1.7/multicard.json", function(data, status){
+        $.get("../assets/js/v1.8/multicard.json", function(data, status){
           multicard = data.multicard;
           for( var i = 0; i < multicard.length; i++ ){
             $("#"+multicard[i].card_idname+"").append("<li class='col-md-3 col-xs-6' onclick='show_page("+i+")'><div class='list-card'><a><img src="+multicard[i].img_src+"></a><article><p>"+multicard[i].card_description+"</p><i></i><span>"+multicard[i].card_area+"</span><a class='btn btn-primary'>See more</a></article></div></li>");
