79fe9981db688a4c917bfce66746d1cb380ee5e6
karthikeyankkn <connecttokkn@hotmail.com>
2018-02-22 17:46:29 +0530

    added rr promoters page

--- assets/js/v1.27/script.js
+++ assets/js/v1.27/script.js
@@ -256,7 +256,7 @@ $(document).ready(function(){
                 "category": categoryName,
                 "Selected_Package" : selected_package,
                 "_subject" : "Get spini partner-with-us!",
-                "_cc" : "karthikeyan@spinircle.com, prathap@spinircle.com, gowrishankar@spinircle.com"
+                "_cc" : "karthikeyan@spinircle.com, prathap@spinircle.com, gowrishankar@spinircle.com, anuradha.v@spinircle.com"
               },
               dataType: "json"
           });
@@ -499,7 +499,7 @@ $(document).ready(function(){
       get_json();
       // debugger
       function get_json(index){
-        $.get("../assets/js/v1.26/multicard.json", function(data, status){
+        $.get("../assets/js/v1.27/multicard.json", function(data, status){
           multicard = data.multicard;
           for( var i = 0; i < multicard.length; i++ ){
             $("#"+multicard[i].card_idname+"").append("<li class='col-md-3 col-xs-6' onclick='show_page("+i+")'><div class='list-card'><a><img src="+multicard[i].img_src+"></a><article><p>"+multicard[i].card_description+"</p><i></i><span>"+multicard[i].card_area+"</span><a class='btn btn-primary'>See more</a></article></div></li>");
