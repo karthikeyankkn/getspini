056e748d8533b233252c789f9a27d142f2d7c655
baskarbss <baskar@spinircle.com>
2018-03-03 17:01:00 +0530

    banner changes

--- assets/js/v1.30/script.js
+++ assets/js/v1.30/script.js
@@ -7,38 +7,38 @@ $(document).ready(function(){
 	windowHight();
     // script for common device image starts
      // images for desktop
-      var desktop_img = [ "https://cdn.getspini.com/optimized/nest-bn.jpg",
+      var desktop_img = [ "https://cdn.getspini.com/banner/Nest-Fortune.jpg",
+                          "https://cdn.getspini.com/optimized/nest-bn.jpg",
                           "https://cdn.getspini.com/optimized/hiranandani-bn.jpg",
                           "https://cdn.getspini.com/optimized/stepsstone-bn.jpg",
                           "../assets/images/banner/Nest_20Kanathur.jpg",
                           "../assets/images/banner/Stepstone_20Prasanna.jpg",
                           "../assets/images/gvspl/GVSPl_20banner.jpg",
                           "https://cdn.getspini.com/banner/Nest-Suraj.jpg",
-                          "https://cdn.getspini.com/banner/Nest-Life.jpg",
-                          "https://cdn.getspini.com/banner/Nest-Fortune.jpg"
+                          "https://cdn.getspini.com/banner/Nest-Life.jpg"
                         ];
 
       // images for mobile
-      var mobile_img = [  "https://cdn.getspini.com/nest-mob-v2-1.jpg",
+      var mobile_img = [  "https://cdn.getspini.com/banner/Nest-Fortune-Mob.jpg",
+                          "https://cdn.getspini.com/nest-mob-v2-1.jpg",
                           "https://cdn.getspini.com/akshaya-today-mob-v2-1.jpg",
                           "https://cdn.getspini.com/stepstone-mob-v2-1.jpg",
                           "../assets/images/banner/Nest_20Mob.jpg",
                           "../assets/images/banner/Stepstone_20Prasanna_20Mob.jpg",
                           "../assets/images/gvspl/GVSPL_20Mob.jpg",
                           "https://cdn.getspini.com/banner/Nest-Suraj-Mob.jpg",
-                          "https://cdn.getspini.com/banner/Nest-Life-Mob.jpg",
-                          "https://cdn.getspini.com/banner/Nest-Fortune-Mob.jpg"
+                          "https://cdn.getspini.com/banner/Nest-Life-Mob.jpg"
                         ];
       // links for slider
-      var slider_link = ["realestate/nest-amaze-Sholinganallur.html",
+      var slider_link = ["realestate/nest-fortune-choolaimedu.html",
+                          "realestate/nest-amaze-Sholinganallur.html",
                          "realestate/hiranandani-parks-orgadam.html",
                          "realestate/stepstone-ananthaya-Urapakkam.html",
                          "realestate/nest-njoy-villa-kanathur.html",
                          "realestate/stepstone-prasanas-apartments-Sholinganallur.html",
                          "realestate/gvspl-coimbatore.html",
                          "realestate/nest-suraj-mahalingapuram.html",
-                         "realestate/nest-life-thiruvanmiyur.html",
-                         "realestate/nest-fortune-choolaimedu.html"
+                         "realestate/nest-life-thiruvanmiyur.html"
                          ];
 
       var window_width, devices, slider_id, alt, parts, astSegment;
