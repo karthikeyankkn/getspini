26a09f9b57b3dd2c25792fed145f154b861ab084
karthikeyankkn <connecttokkn@hotmail.com>
2018-04-23 13:58:26 +0530

    renamed js

--- assets/js/v1.30.22/script.js
+++ assets/js/v1.30.22/script.js
@@ -539,7 +539,7 @@ $(document).ready(function(){
       get_json();
       // debugger
       function get_json(index){
-        $.get("../assets/js/v1.30.21/multicard.json", function(data, status){
+        $.get("../assets/js/v1.30.22/multicard.json", function(data, status){
           multicard = data.multicard;
           for( var i = 0; i < multicard.length; i++ ){
             $("#"+multicard[i].card_idname+"").append("<li class='col-md-3 col-xs-6' onclick='show_page("+i+")'><div class='list-card'><a><img src="+multicard[i].img_src+"></a><article><p>"+multicard[i].card_description+"</p><i></i><span>"+multicard[i].card_area+"</span><a class='btn btn-primary'>See more</a></article></div></li>");
@@ -688,7 +688,7 @@ $(document).ready(function(){
     card_limit_end = 20; 
     get_json();
     function get_json(index){
-      $.get("../assets/js/v1.30.21/cards.json", function(data, status){
+      $.get("../assets/js/v1.30.22/cards.json", function(data, status){
         cards = data.cards;
         
         $(".see-more-pagination").click(function(){
