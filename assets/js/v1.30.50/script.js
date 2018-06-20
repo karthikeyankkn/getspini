83afeca4589257177f65ff705b53f991778e7d56
karthikeyankkn <connecttokkn@hotmail.com>
2018-06-20 16:09:45 +0530

    added shabudeen mail id

--- assets/js/v1.30.50/script.js
+++ assets/js/v1.30.50/script.js
@@ -273,7 +273,7 @@ $(document).ready(function() {
      "category": categoryName,
      "Selected_Package": selected_package,
      "_subject": "Get spini partner-with-us!",
-     "_cc": "karthikeyan@spinircle.com, anand.kumar@spinircle.com, gowrishankar@spinircle.com, anuradha.v@spinircle.com"
+     "_cc": "karthikeyan@spinircle.com, anand.kumar@spinircle.com, gowrishankar@spinircle.com, anuradha.v@spinircle.com, shabuddeen@spinircle.com"
     },
     dataType: "json"
    });
@@ -539,7 +539,7 @@ $(document).ready(function() {
      "Estimatedcost": estimatedcost,
      "Rangecost": rangecost,
      "_subject": "Lead from Interior Estimation Page !",
-     "_cc": "karthikeyan@spinircle.com, anand.kumar@spinircle.com, anuradha.v@spinircle.com, joshua.simon@spinircle.com"
+     "_cc": "karthikeyan@spinircle.com, anand.kumar@spinircle.com, anuradha.v@spinircle.com, shabuddeen@spinircle.com, joshua.simon@spinircle.com"
     },
     dataType: "json"
    });
