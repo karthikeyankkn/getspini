1ba6e252243165ad54087459a09868d4cb071a8e
karthikeyankkn <connecttokkn@hotmail.com>
2018-10-15 10:19:45 +0530

    renamed js and css

--- assets/js/v1.30.59/backupjs.js
+++ assets/js/v1.30.59/backupjs.js
@@ -705,7 +705,7 @@ $(document).ready(function() {
     
 
     function get_json(index) {
-        $.get("../assets/js/v1.30.58/" + card_category + ".json", function(data, status) {
+        $.get("../assets/js/v1.30.59/" + card_category + ".json", function(data, status) {
             // debugger
             cards = data.cards;
             cards = cards.reverse();
